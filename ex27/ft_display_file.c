1f9a0623e33e0ed64ec26c8ed040c824a976bf1d
Joao Barbosa De Meneses Correia <joaobarb@c1r13s8.42porto.com>
2025-04-07 17:28:38 +0100

    updated ex27

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: joaobarb <joaobarb@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 15:48:00 by joaobarb          #+#    #+#             */
-/*   Updated: 2025/04/07 16:27:29 by joaobarb         ###   ########.fr       */
+/*   Updated: 2025/04/07 17:25:40 by joaobarb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,17 @@
 void	ft_write_file(char *filename)
 {
 	int		fd;
-	int		br;
 	char	buffer;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		write(2, "Cannot read file.\n", 19);
+		write(2, "Cannot read file.\n", 18);
 		return ;
 	}
-	br = read(fd, &buffer, 1);
-	while (br > 0)
+	while (read(fd, &buffer, 1))
 	{
 		write(1, &buffer, 1);
-		br = read(fd, &buffer, 1);
 	}
 	close(fd);
 }
@@ -37,9 +34,9 @@ void	ft_write_file(char *filename)
 int	main(int argc, char *argv[])
 {
 	if (argc < 2)
-		write(2, "File name missing.\n", 20);
+		write(2, "File name missing.\n", 19);
 	else if (argc > 2)
-		write(2, "Too many arguments.\n", 21);
+		write(2, "Too many arguments.\n", 20);
 	else if (argc == 2)
 		ft_write_file(argv[1]);
 	return (0);
